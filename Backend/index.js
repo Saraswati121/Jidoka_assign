fcda192c8c58d6244861ee13169af2764deb473e
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-11-10 21:05:11 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -1,38 +1,24 @@
 const express = require("express");
 const http = require("http");
-const io = require("socket.io");
-
+const {Server} = require("socket.io");
 
 const app = express();
-const httpServer = http.createServer(app);
-const server = new io.Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-let timeChange;
-const data = [
-  { name: 1, x: Math.random() * 10, y: Math.random() * 10 },
-  { name: 2, x: Math.random() * 10, y: Math.random() * 10 },
-  { name: 3, x: Math.random() * 10, y: Math.random() * 10 },
-  { name: 4, x: Math.random() * 10, y: Math.random() * 10 },
-  { name: 5, x: Math.random() * 10, y: Math.random() * 10 },
-];
-server.on("connection", (socket) => {
-  if (timeChange) clearInterval(timeChange);
-  if (data.length > 5) {
-    data.reverse().pop();
-    data.reverse();
-  }
-  data.push({
-    name: data[data.length - 1].name + 1,
-    x: Math.random() * 10,
-    y: Math.random() * 10,
-  });
-  setInterval(()=> socket.emit("message", data), 1000);
-});
+const webServer = http.createServer(app);
+const wss= new Server(webServer)
 
-httpServer.listen(8080, () => {
+const history=[]
+wss.on("connection", () => {
+  ws.broadcast.emit("new user")
+  ws.emit("history",history)
+
+  ws.on("new message",(m)=>{ 
+      // console.log("got new message",m)
+      history.push(m)
+      wss.emit("new message",m)
+  })
+})
+
+
+webServer.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
